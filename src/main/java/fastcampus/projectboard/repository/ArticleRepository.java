74cdfb3dfe08eb8de124d2af0f9470eedeb19dcf
leejongsuu <lik9819@naver.com>
2024-03-22 19:41:21 +0900

    #1 - 필요하지 않은 import 제거.

--- src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
@@ -10,8 +10,6 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.time.DateTimeException;
-
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
