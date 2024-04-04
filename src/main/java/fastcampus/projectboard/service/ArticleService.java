abe23e531fa086b595ee544a6f89999e3578a706
leejongsuu <lik9819@naver.com>
2024-04-04 10:19:19 +0900

    #24 - 게시판 페이지 기능 테스트 정의
    검색, 각 게시글 페이지로 이동, 페이지네이션, 홈 리다이렉션, 정렬 기능 테스트를 정의했다.

---
+++ src/main/java/fastcampus/projectboard/service/ArticleService.java
@@ -0,0 +1,27 @@
+package fastcampus.projectboard.service;
+
+import fastcampus.projectboard.domain.type.SearchType;
+import fastcampus.projectboard.dto.ArticleDto;
+import fastcampus.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> serachArticles(SearchType searchType, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+}
