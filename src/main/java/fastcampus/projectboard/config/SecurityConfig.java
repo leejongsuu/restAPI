65551028f7ab5210b9853ee0477f3bd751459ac7
leejongsuu <lik9819@naver.com>
2024-04-08 09:42:41 +0900

    SecurityConfig - 코드 리팩토링

--- src/main/java/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/fastcampus/projectboard/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
+        return http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers(
@@ -27,7 +27,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/").permitAll());
-        return http.build();
+                        .logoutSuccessUrl("/").permitAll())
+                .build();
     }
 }
